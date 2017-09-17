4ac57151165864c8ee5f7062e3bf9dc55cd140a1
ghost <kirk.douglas.richardson@gmail.com>
2017-09-17 13:43:06 -0600

    button functionality

--- src/App.js
+++ src/App.js
@@ -4,30 +4,46 @@ import { Button, ButtonGroup } from 'react-bootstrap';
 import $ from 'jquery'
 import RecipeModal from './comp/recipeModal.js'
 
-/*
-App
- - recipe
-    - title
-    - ingredientson
-    - delete button
-    - edit button
-*/
-
-const recipes = [
-  {title: "Ribs", ingredients: ["meat", "onions", "poo"]},
-  {title: "RibsBig", ingredients: ["bigmeat", "onions", "poo"]},
-  {title: "RibsSmall", ingredients: ["smallmeat", "onions", "poo"]}
-
-];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false
+      showModal: false,
+      recipes: [],
+      title: '',
+      ingredients: []
     }
   }
 
+  componentDidMount() {
+    // check storage & assign to recipes variable the contents if not null, else use default
+    const storage = JSON.parse(localStorage.getItem('recipes'));
+    let storedRecipes;
+    // set default value if recipes aren't already stored
+    const recipes = storage.length !== 0 ? storage : [
+      { id: 1234,
+        title:
+          "Chocolate Chip Cookies",
+        ingredients: [
+          "1/2 cup (1 stick) unsalted butter",
+          "3/4 cup packed dark brown sugar",
+          "3/4 cup sugar",
+          "2 large eggs",
+          "1 teaspoon pure vanilla extract",
+          "1 (12-ounce) bag semisweet chocolate chips, or chunks",
+          "2 1/4 cups all-purpose flour",
+          "3/4 teaspoon baking soda",
+          "1 teaspoon fine salt"] }
+    ];
+
+    localStorage.setItem('recipes', JSON.stringify(recipes))
+    storedRecipes = JSON.parse(localStorage.getItem('recipes'));
+    this.setState({ recipes: storedRecipes });
+  }
+
+
+  // modal switches
   openModal = () => {
     this.setState({ showModal: true });
   }
@@ -35,27 +51,90 @@ class App extends Component {
     this.setState({ showModal: false })
   }
 
+  // handle user input to the modal in state
+  changeState = (e) => {
+    let value = e.target.value;
+    if (e.target.id === "title")
+      this.setState({ title: value });
+    else if (e.target.id === "ingredients") {
+      value = value.split(',');
+      this.setState({ ingredients: value });
+    }
+  }
+
+
+  // add new recipe to local storage & clear placeholders in state
+  addRecipe = () => {
+    const newId = Math.floor(Math.random() * 9999);
+    let storedRecipes = JSON.parse(localStorage.getItem('recipes'));
+    let newRecipe = {id: newId, title: this.state.title, ingredients: this.state.ingredients};
+    storedRecipes.push(newRecipe);
+    localStorage.setItem('recipes', JSON.stringify(storedRecipes));
+
+    this.setState({
+      title: '',
+      ingredients: [],
+      showModal: false,
+      recipes: storedRecipes
+    });
+  }
+
+
+deleteRecipe = (e) => {
+  // get the id of the recipeBox parent
+  const idToRemove = parseInt(e.target.parentElement.parentElement.id, 10)
+  const storedRecipes = JSON.parse(localStorage.getItem('recipes'));
+  const newRecipes = storedRecipes.filter((obj) => obj.id !== idToRemove);
+  localStorage.setItem('recipes', JSON.stringify(newRecipes));
+  this.setState({recipes: newRecipes});
+}
+
+
+// editRecipe = (e) => {
+//   const idToEdit = parseInt(e.target.parentElement.parentElement.id, 10)
+//   let storedRecipes = JSON.parse(localStorage.getItem('recipes'));
+//   let recipe = storedRecipes.filter((obj) => obj.id === idToEdit)[0];
+//
+//
+//   console.log(idToEdit, storedRecipes, recipe, recipe.title, recipe.ingredients);
+//   this.setState({ showModal: true })
+//
+//   //
+//   // localStorage.setItem('recipes', JSON.stringify(newRecipes));
+//   // this.setState({recipes: newRecipes});
+// }
+
+
+
   render() {
-    const recipeList = recipes.map((recipe) => {
+    const recipeList = this.state.recipes.map((recipe) => {
       return (
         <RecipeBox
-          key={recipe.title}
+          id={recipe.id}
+          key={recipe.id}
           title={recipe.title}
           ingredients={recipe.ingredients}
-          openModal={this.openModal} />
+          openModal={this.openModal}
+          editRecipe={this.editRecipe}
+          deleteRecipe={this.deleteRecipe}/>
       );
       });
 
+
     return (
       <div className="App">
-        <h1>Recipe Repo</h1>
-        <AddButton className="addButton" openModal={this.openModal}/>
+        <h1>RECIPE REPO</h1>
+        <AddButton className="addButton"
+          openModal={this.openModal}
+          />
 
         { recipeList }
 
         <RecipeModal
           showModal={this.state.showModal}
-          closeModal={this.closeModal} />
+          closeModal={this.closeModal}
+          changeState={this.changeState}
+          addRecipe={this.addRecipe}/>
 
       </div>
     );
@@ -77,19 +156,19 @@ class App extends Component {
 // appended as an object to the end of an array
 
 const RecipeBox = (props) => {
-  const title = props.title;
   return (
     <div className="recipe">
 
-      <h3 className="recipeTitle" onClick={() => $('#' + title).toggle(500)}>
+      <h3 className="recipeTitle"
+        onClick={() => $('#' + props.id).toggle(500)}>
         {props.title}
       </h3>
 
-      <div className="recipeBody" id={props.title}>
+      <div className="recipeBody" id={props.id}>
       <Ingredients ingredients={props.ingredients} />
-        <ButtonGroup bsSize="md">
-          <EditButton />
-          <DeleteButton />
+        <ButtonGroup className="btnGroup" bsSize="lg">
+          <EditButton editRecipe={props.editRecipe}/>
+          <DeleteButton deleteRecipe={props.deleteRecipe}/>
         </ButtonGroup>
       </div>
 
@@ -106,7 +185,7 @@ const Ingredients = (props) => {
 
 const DeleteButton = (props) => {
   return (
-       <Button bsStyle="danger" onClick={() => alert('hello')}>
+       <Button bsStyle="danger" onClick={props.deleteRecipe}>
       <i className="glyphicon glyphicon-trash"></i> Delete
     </Button>
   );
@@ -116,7 +195,7 @@ const DeleteButton = (props) => {
 
 const EditButton = (props) => {
   return (
-    <Button bsStyle="primary" onClick={() => alert('hello')}>
+    <Button bsStyle="primary" onClick={props.editRecipe}>
       <i className="glyphicon glyphicon-pencil"></i> Edit
     </Button>
   );
@@ -124,7 +203,10 @@ const EditButton = (props) => {
 
 const AddButton = (props) => {
   return (
-    <Button className="addButton" bsStyle="success" onClick={props.openModal}>
+    <Button className="addButton"
+      bsSize="lg"
+      bsStyle="success"
+      onClick={props.openModal}>
       <i className="glyphicon glyphicon-plus"></i> Add
     </Button>
   );
