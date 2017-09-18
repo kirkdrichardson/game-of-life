f50ef2c6e8d50920fb6dec240ea354711573574e
ghost <kirk.douglas.richardson@gmail.com>
2017-09-17 18:22:06 -0600

    add, delete recipes

--- src/comp/recipeModal.js
+++ src/comp/recipeModal.js
@@ -7,22 +7,27 @@ class RecipeModal extends Component {
     super(props);
   }
   render() {
+    const buttonText = this.props.action === 'Add Recipe' ? "Save" : "Save Edits"
+
     return (
       <Modal show={this.props.showModal} onHide={this.props.closeModal}>
         <Modal.Header closeButton>
-          <Modal.Title style={{color: "black"}}>
-            <ControlLabel>Title</ControlLabel>
-            <FormGroup>
-              <FormControl
-                type="text"
-                id="title"
-                placeholder="Peach Cobbler"
-                onChange={ this.props.changeState } />
-            </FormGroup>
+          <Modal.Title>
+            {this.props.action}
           </Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
+
+        <ControlLabel>Title</ControlLabel>
+        <FormGroup>
+          <FormControl
+            type="text"
+            id="title"
+            placeholder="Peach Cobbler"
+            onChange={ this.props.changeState } />
+        </FormGroup>
+
         <FormGroup>
           <ControlLabel>Ingredients</ControlLabel>
           <FormControl
@@ -36,7 +41,8 @@ class RecipeModal extends Component {
 
         <Modal.Footer>
           <Button bsStyle="success" type="submit" onClick={this.props.addRecipe}>
-            <i className="glyphicon glyphicon-check"></i> Save
+            <i className="glyphicon glyphicon-check"></i>
+            { buttonText }
           </Button>
         </Modal.Footer>
       </Modal>
