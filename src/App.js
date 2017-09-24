743320c4343f265d24c8d3e8affb55d428c34004
ghost <kirk.douglas.richardson@gmail.com>
2017-09-23 20:00:38 -0600

    generate fx working

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class App extends Component {
     this.state = {
       boardArr: [],  // 2D array of components aligned in columns & rows
       componentsArr: [],
-      height: 10,
-      width: 10,
+      height: 30,
+      width: 50,
       running: true,
       generation: 0
     }
@@ -30,84 +30,65 @@ class App extends Component {
 
 
 
+
   componentDidMount() {
 
-      // load a random configuration by default
-      // if (this.state.generation === 0) {
-      //   this.setState({
-      //     boardArr: this.createRandomBoard()
-      //   }, () => this.setState({componentsArr: this.generate().componentsArr}));
-      // }
+    if (this.state.generation === 0) {
+      this.setState({ boardArr: this.createRandomBoard() })
+    }
 
+    if (this.state.running) {
+      this.generateId = setInterval(this.generate, 300);
+    }
 
-      // currently does not render a board, although the state does get updated with a board
-      // I suspect the component doesn't rerender when state changes
-
-      // look into lifecycle methods so that this code is run whenever state changes
-      //
-
-      if (this.state.running) {
-
-        if (this.state.boardArr.length === 0) {
-
-          this.setState({
-            boardArr: this.createRandomBoard()
-          });
-        }
-
-        else if (this.state.boardArr.length > 0) {
-
-          const boardObj = this.generate();
-          this.setState({
-            boardArr: boardObj.newBoard
-          }, () => this.setState({
-            componentsArr: boardObj.componentsArr,
-            generation: this.state.generation + 1
-          }));
-        }
-      }
-
-
-    //   // continuously generate new boards if running
-    //  if (this.state.running) {
-    //    const boardObj = this.generate();
-    //    console.log(boardObj)
-    //    const components = boardObj.componentsArr; // used to render board
-    //    const newBoard = boardObj.newBoard; // used to conditionally assign cell values
-     //
-    //    this.setState({
-    //      boardArr: newBoard,
-    //      componentsArr: components
-    //    });
-    //  }
   }
 
 
 
   // on click, switches to cell to opposite state by toggling class
   toggleCell = (e) => {
-    const classArr = e.target.classList;
-    if (classArr.contains("dead")) {
-      classArr.remove("dead");
-      classArr.add("alive");
-    }
-    else {
-      classArr.remove("alive");
-      classArr.add("dead");
-    }
+      // cell id is str of indices in form "row,cell"
+      const idx = e.target.id.split(',');
+      let board = this.state.boardArr;
+       // if cell was 0, cell == 1, else cell == 0
+      let cell = board[parseInt(idx[0])][parseInt([idx[1]])] ? 0 : 1;
+      board[parseInt(idx[0])][parseInt([idx[1]])] = cell;
+      this.setState({ boardArr: board });
   }
 
 
 
   toggleStart = () => {
+    if (this.state.running) {
+      clearInterval(this.generateId);
+    }
+    else {
+      this.generateId = setInterval(this.generate, 500);
+    }
     this.setState({ running: !this.state.running });
   }
+
+
+
 
   reset = () => {
     const board = this.createRandomBoard();
     this.setState({ generation: 0, boardArr: board })
   }
 
+
+
+  clear = () => {
+    let board = [];
+    for (let row = 0; row < this.state.height; row++) {
+      let rowArr = [];
+      for (let column = 0; column < this.state.width; column++) {
+        rowArr.push(0);
+      }
+      board.push(rowArr);
+    }
+    this.setState({ boardArr: board});
+  }
 
 
   // generates a random board state to be mapped through
@@ -124,21 +105,18 @@ class App extends Component {
   }
 
 
-// return arr of the values of the 8 cells surrounding current cell
-// if row or cell is on edge, returns an empty arr
-  getNeighborsArr = (row, cell) => {
-    const selfId = `${row}${cell}`;
-    const board = this.state.boardArr;
-    let neighborsArr = [];
 
-    // from a starting cell, check the top, middle, & bottom rows
+    // arguments: the current board (arr), and the indices of a row & cell (int)
+    // returns sum of the values of the 8 cells surrounding current cell
+    // if row or cell is on edge, returns an empty arr
+  getNeighborsSum = (board, row, cell) => {
+    const selfId = `${row}${cell}`;
+    let neighborsArr = [];
+      // from a starting cell, check the top, middle, & bottom rows
     for (let i = -1; i <= 1; i++) {
       // check the left, middle, & right cells
       for (let j = -1; j <= 1; j++) {
-        // if that cell is defined, push to return arr
-
         let neighbor, cellId;
-
         // if not the top or bottom row, assign indices for the 3 neighbor rows
         if ( (row - 1) >= 0 && (row + 1) <= (this.state.height - 1) ) {
           let neighborRow = board[row + i]; // assign row if not the first or last arr in board
@@ -155,7 +133,7 @@ class App extends Component {
         }
       }
     }
-    return neighborsArr;
+    return neighborsArr.reduce((sum, cell) => sum + cell, 0);
   }
 
 // if reduced neighbors arr > 3, die; if === 3, live; if < 2, die
@@ -169,46 +147,44 @@ class App extends Component {
 
 // map over boardArr of binary values & return new binary board & components arr in obj
   generate = () => {
-    const prevGen = this.state.boardArr;
+    // if boardArr is defined, use it, else use a random configuration
+    const prevGenBinary = this.state.boardArr.length ? this.state.boardArr : this.createRandomBoard();
+    let newGenBinary = [], rowArr, sumOfNeighbors;
 
-    const newGen =
-      prevGen.map((row, rowIdx) => {
-        let cellRow = [];
+    for (var i = 0; i < prevGenBinary.length; i++) {
+      rowArr = [];
+      for (var j = 0; j < prevGenBinary[i].length; j++) {
 
-        row.forEach((cell, cellIdx) => {
+        sumOfNeighbors = this.getNeighborsSum(prevGenBinary, i, j);
+          // if < 2 neighbors, cell dies (underpopulation); also handle edge cases (where neighborsArr.length = 0)
+        if (sumOfNeighbors < 2) {
+          rowArr.push(0);
+        }
+         // if 2 neighbors, retain cell's existing value (equilibrium)
+        else if (sumOfNeighbors === 2) {
+          rowArr.push(prevGenBinary[i][j]);
+        }
+          // if 3 neighbors, it is either born or remains alive (reproduction)
+        else if (sumOfNeighbors === 3) {
+          rowArr.push(1);
+        }
+          // if more than 3 neighbors, cell dies (overpopulation)
+        else if (sumOfNeighbors > 3) {
+          rowArr.push(0);
+        }
+          // error handling. Should not run.
+        else {
+          console.log("Error calculating neighbors in generate Fx");
+          rowArr.push(0);
+        }
+      }
+      newGenBinary.push(rowArr);
+    }
 
-
-          let temp = this.getNeighborsArr(rowIdx, cellIdx);
-          if (temp.length > 0) {
-            console.log(`neighbors at [${rowIdx}, ${cellIdx}] are ${temp}`);
-
-          }
-
-
-          // pass binary value of cell as props & push to a row
-          cellRow.push(<Cell
-            key={"cell-" + cellIdx}
-            status={cell}
-            toggleCell={this.toggleCell} />
-          );
-
-
-
-        });
-        return (
-          <Row
-            key={"row-" + rowIdx}
-            className="Row"
-            cellsArr={cellRow}
-            toggleCell={this.toggleCell}
-            />
-        );
-      });
-
-      return {
-        "componentsArr": newGen,
-        "newBoard": []
-      };
+    this.setState({
+      boardArr: newGenBinary,
+      generation: this.state.generation + 1
+    });
   }
 
 
@@ -219,28 +195,50 @@ class App extends Component {
     return (
       <div className="App">
 
-      <h3>Generation: {this.state.generation}</h3>
+      <h3>Generation: <span style={{color: "red"}}>{this.state.generation}</span></h3>
 
         <div className="container">
           <table className="Board">
             <tbody>
 
-            { this.state.componentsArr }
+            { /* generate components from 2D boardArr. if cell == 0, dead, else alive */ }
+            { this.state.boardArr.map((row, rowIdx) => {
+              let cellRow = [];
+              row.forEach((cell, cellIdx) => {
+                // pass binary value of cell as props & push to a row
+                cellRow.push(<Cell
+                  id={`${rowIdx},${cellIdx}`}
+                  key={"cell-" + cellIdx}
+                  status={cell}
+                  toggleCell={this.toggleCell} />
+                );
+              });
+              return (
+                <Row
+                  key={"row-" + rowIdx}
+                  className="Row"
+                  cellsArr={cellRow}
+                  toggleCell={this.toggleCell}
+                  />
+              );
+            }) }
 
             </tbody>
             </table>
         </div>
 
-        <button
-          onClick={this.toggleStart}
-          className="btn">
-          {!this.state.running && "Start" || this.state.running && "Pause"}
+
+        <button onClick={this.toggleStart} className="btn">
+          {!this.state.running ? "Start" : "Pause"}
         </button>
 
-        <button
-        onClick={this.reset}
-        className="btn">
-        Reset</button>
+        <button onClick={this.reset} className="btn">
+          Reset
+        </button>
+
+        <button onClick={this.clear} className="btn">
+          Clear
+        </button>
 
       </div>
     );
